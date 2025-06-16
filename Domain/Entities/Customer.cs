b92087ef64067fa96755eb4c990a3767bf7b1e83
Rahelaraya <rahelaraya26@gmail.com>
2025-06-16 03:21:22 +0200

    Klar med Domain

---
+++ Domain/Entities/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }    // Nullable
+        public string? Email { get; set; }   // Nullable
+        public string? Phone { get; set; }   // Nullable
+        public string? Address { get; set; } // Nullable
+
+
+    }
+}
